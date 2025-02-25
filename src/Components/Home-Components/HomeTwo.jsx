a58b83f51a37a16b5833b238462fe9afb91bc8a2
kingsLib5 <libertykings88@gmail.com>
2025-02-25 10:21:12 +0100

    Home Page component two update

---
+++ src/Components/Home-Components/HomeTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomeTwo() {
+  return (
+    <>
+      <div className=' bg-amber-400 grid grid-rows-2 h-[60vh]'>
+            <div className=' bg-amber-600'></div>
+            <div className=' bg-amber-800'></div>
+      </div>
+    </>
+  )
+}
+
+export default HomeTwo
