5d1fb8711721722c0c861bd7311dc6fa9195a1cc
lexy-babay <mesomaonubeze@gmail.com>
2025-02-28 11:38:31 +0100

    update

--- src/Components/Contactus-Components/Contact2.jsx
+++ src/Components/Contactus-Components/Contact2.jsx
@@ -167,19 +167,3 @@ function Contact2() {
 }
 
 export default Contact2;
-
-    <>
-      <div className=' bg-white min-h-[90vh] grid grid-rows-12'>
-        <div className='  grid row-span-1 ml-[50px] items-center '>
-            
-             <h4> <a href="/"><span className=' text-gray-400'>Home</span></a>  <span className=' text-gray-200'>/</span> Contact</h4>
-            
-        </div>
-        <div className=' bg-amber-500 row-span-6'></div>
-        <div className=' bg-amber-800 row-span-5'></div>
-      </div>
-    </>
-  )
-}
-
-export default Contact2
