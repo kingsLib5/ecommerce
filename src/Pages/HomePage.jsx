4b864ef6986a3788d3bae346ca6c02477717c286
kingsLib5 <libertykings88@gmail.com>
2025-03-03 10:34:27 +0100

    home page updates

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Home from "../Components/Home-Components/Home"
 import HomeTwo from '../Components/Home-Components/HomeTwo'
 import HomeThree from '../Components/Home-Components/HomeThree'
+import HomeFour from '../Components/Home-Components/HomeFour'
+import HomeFive from '../Components/Home-Components/HomeFive'
+import HomeSix from '../Components/Home-Components/HomeSix'
 
 function HomePage() {
   return (
@@ -9,6 +12,9 @@ function HomePage() {
       <Home/>
       <HomeTwo/>
       <HomeThree/>
+      <HomeFour/>
+      <HomeFive/>
+      <HomeSix/>
 
     </>
   )
